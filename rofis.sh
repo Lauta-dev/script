cec7567d535ff10ea2d056a8f9d3e82c1f193013
lauta <altairdiaz245@gmail.com>
2024-07-15 19:30:24 -0300

    change style

--- rofis.sh
+++ rofis.sh
@@ -3,11 +3,9 @@
 # Script para lanzar rofi sin tener que tocar la configuración del Window Manager
 # Rofi themes (No es mío): https://github.com/adi1090x/rofi
 
-user_home="/home/lauta"
-
-dir_rofi="$user_home/.config/rofi"
-launch_apps="rofi -show drun -theme $dir_rofi/launchers/type-3/style-2.rasi"
-powermenu="$dir_rofi/applets/bin/powermenu.sh"
+dir_rofi="$HOME/.config/rofi"
+launch_apps="rofi -show drun -theme $dir_rofi/launchers/type-2/style-2.rasi"
+powermenu="$dir_rofi/powermenu/type-2/powermenu.sh"
 
 case "$1" in
   'apps') $launch_apps
