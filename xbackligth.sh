d7a0406164f6409f5096d742c40c488f6122228b
lauta <altairdiaz245@gmail.com>
2024-07-24 15:49:58 -0300

    fix: %

--- xbackligth.sh
+++ xbackligth.sh
@@ -23,7 +23,7 @@ trigger_notify() {
   dunstify -u low \
     -r 2 \
     -i $icon \
-    "Brightness: $brightness_status%"
+    "Brightness: $brightness_status"
 }
 
 if [[ "$brightness_status" > "0" && "$brightness_status" < "25" ]]; then
