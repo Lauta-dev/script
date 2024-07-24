f0ceef46a2cd0dc8c5bbb38226c6354df4fe89ce
lauta <altairdiaz245@gmail.com>
2024-07-24 15:49:27 -0300

    add view window

--- rofis.sh
+++ rofis.sh
@@ -6,11 +6,14 @@
 dir_rofi="$HOME/.config/rofi"
 launch_apps="rofi -show drun -theme $dir_rofi/launchers/type-2/style-2.rasi"
 powermenu="$dir_rofi/powermenu/type-2/powermenu.sh"
+view_windows="rofi -show window -theme $dir_rofi/launchers/type-2/style-2.rasi"
 
 case "$1" in
   'apps') $launch_apps
   ;;
   'powermenu') $powermenu
+  ;;
+  'windows') $view_windows
   ;;
 esac
 
